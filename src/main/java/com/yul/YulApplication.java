b218ab2965ba3163618709f4b07334df2f866b36
yul2 <eyul9313@gmail.com>
2024-06-29 13:42:56 +0900

    resume start

---
+++ src/main/java/com/yul/YulApplication.java
@@ -0,0 +1,13 @@
+package com.yul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YulApplication.class, args);
+	}
+
+}
